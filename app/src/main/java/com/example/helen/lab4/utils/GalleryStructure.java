87bbc93d3ae536ee693219b3890560a4cc3638ec
helen <helen>
2018-06-08 17:07:40 -0400

    lab

---
+++ app/src/main/java/com/example/helen/lab4/utils/GalleryStructure.java
@@ -0,0 +1,19 @@
+package com.example.helen.lab4.utils;
+
+public class GalleryStructure {
+    private String des;
+    private Integer id;
+    public  void setDes(String des){
+        this.des= des;
+    }
+    public String getDes (){
+        return this.des;
+    }
+    public void setId(Integer id){
+        this.id=id;
+    }
+    public Integer getId (){
+        return this.id;
+    }
+
+}
